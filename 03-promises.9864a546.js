b56e7f23dbe5e96e1fc65c609455ea644bc68fd4
OlenaHras <100197147+OlenaHras@users.noreply.github.com>
2022-08-13 09:49:59 +0000

    Deploying to gh-pages from @ OlenaHras/goit-js-hw-09@e8e5f0f7ac9a83052bd711bf6a3c1dd01867f790 🚀

--- 03-promises.9864a546.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button").addEventListener("click",(function(e,r){Math.random()>.3||reject("error")})),document.querySelector('[name="delay"]'),document.querySelector('[name="delay"]'),document.querySelector('[name="delay"]');new Promise(((e,r)=>{}));
-//# sourceMappingURL=03-promises.9864a546.js.map
--- 03-promises.c1e4208e.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("button").addEventListener("click",(function(e,n){Math.random()>.3||reject("error")})),document.querySelector('[name="delay"]'),document.querySelector('[name="delay"]'),document.querySelector('[name="delay"]');new Promise((function(e,n){}))}();
-//# sourceMappingURL=03-promises.c1e4208e.js.map
